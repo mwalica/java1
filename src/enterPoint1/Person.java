2ec00d4b9776a7328928427764efc564746c4442
Marek Walica <mwalica@gmail.com>
2021-08-25 19:25:26 +0200

    change in Person.java

--- src/enterPoint1/Person.java
+++ src/enterPoint1/Person.java
@@ -1,7 +1,7 @@
 package enterPoint1;
 
 public class Person {
-    void main(String[] args) {
+    public void main(String[] args) {
         System.out.println("Z tego punktu program się nie uruchomi");
     }
 }
