1a00f8e9e0da41055a7239e7e8547227116f47df
Marek Walica <mwalica@gmail.com>
2021-09-04 15:43:41 +0200

    changes in finalVar

---
+++ src/temp/ExampleVar.java
@@ -0,0 +1,11 @@
+package temp;
+
+public class ExampleVar {
+    public static void main(String[] args) {
+        int num1 = 22;
+        var num2 = 33;
+        String str;
+        var name = "Marek";
+        int num3 = 28;
+    }
+}
