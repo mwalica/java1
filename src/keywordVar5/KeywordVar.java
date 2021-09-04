49578ea0789b7e0f9c90d33c0454fa581c108d2f
Marek Walica <mwalica@gmail.com>
2021-09-04 15:50:12 +0200

    var keyword 2

--- src/keywordVar5/KeywordVar.java
+++ src/keywordVar5/KeywordVar.java
@@ -1,9 +1,11 @@
 package keywordVar5;
 
 public class KeywordVar {
+
+    //var num3 = 34 wnioskowanie dzial tylko dla zmiennych lokalnych -> tutaj nie zadziala
     public static void main(String[] args) {
         int num1 = 22;
         var num2 = 3.22; //wnioskowanie typow zadziala
-//        var num3; to jest niedozwolone
+//        var num3; to jest niedozwolone przy var zmienna musi byc od razu zainicjalizowana
     }
 }
